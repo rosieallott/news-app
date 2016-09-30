fecb96af282b83ca92327fe6d06187c71b315773
ercekal <ercekalabalikoglu@gmail.com>
2016-09-30 12:37:52 +0100

    webtitle and article summaries are added with iteration

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ ready(function(){
        var obj = JSON.parse(request.response);
        var first = obj.response.results[0].blocks.body[0].bodyTextSummary;
        var fullText = obj.response.results[1].blocks.body[0].bodyHtml;
-       console.log(obj.response.results[0]);
+       console.log(obj.response.results[1]);
+
          if (request.readyState == XMLHttpRequest.DONE ) {
             if (request.status == 200) {
-              var articleDiv = document.getElementById("news-headline");
-              articleDiv.innerHTML = fullText;
+              for (i = 0; i < 10; i++) {
+                document.getElementsByTagName("a")[i].innerHTML =obj.response.results[i].webTitle;
+                document.getElementsByTagName("p")[i].innerHTML =obj.response.results[i].blocks.body[0].bodyHtml;
+              }
             }
             else if (request.status == 400) {
                alert('There was an error 400');
